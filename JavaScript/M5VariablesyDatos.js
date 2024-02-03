751fe758a208c9efe1461431f6c4d82faad55027
NachoMolina <juanignaciomolina12@gmail.com>
2024-02-03 11:44:22 -0300

    Avances hasta el momento en el prep course

---
+++ JavaScript/M5VariablesyDatos.js
@@ -0,0 +1,23 @@
+var cajon = 11.75;
+
+console.log(typeof(cajon));
+
+// String
+var bootcamp = 'Henry';
+
+// Números
+var numeroEntero = 17;
+var numeroNegativo = -4;
+var numeroDecimal = 11.75;
+
+// Booleanos
+var estoyEntendiendo = true;
+var estoyAburrido = false;
+
+// Undefined
+var cajon1;
+console.log(cajon1);
+
+// Null
+var sinNada = null;
+
